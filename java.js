b34aae08d6f08757041c57791d98ac977c943085
Ronnel Bautista <ronnel.j.bautista@gmail.com>
2017-01-30 11:47:36 -0800

    Declare Assignment

---
+++ java.js
@@ -0,0 +1,24 @@
+    //
+    // Declare variables of the following types:
+    //     4 Numbers
+    //     3 Strings
+    //     2 Booleans
+    //     1 Undefined Variable
+    // Give the variables descriptive names (ex: string1 and string2)
+    // Print each one to the console with a description (ex: console.log("The First String:", string1);)
+    // Try to be creative! (don't use 1, 2, 3, and 4 for your numbers -- test out negatives or decimals!)
+var num = 0;
+var num1 = 1;
+var num2 = 1.25;
+var num3 = -120;
+console.log (num, num1, num2, num3);
+
+var stone = 'stone1, ston2 stone3';
+console.log(stone);
+
+var boolean = true;
+var boolean = false;
+console.log(boolean);
+
+var num = ''
+console.log(num);
